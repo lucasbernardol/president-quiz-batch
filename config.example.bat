f528aa0270bcc9ca0f908e807760b1896fb53a6d
lucasbernardol <jose.lukass03@gmail.com>
2022-03-30 15:09:21 -0300

    Update config.example.bat file && fix typos

--- config.example.bat
+++ config.example.bat
@@ -8,13 +8,13 @@ REM Arquivo de personalização/configurações
 :: palavras, "config.example.bat" para "config.bat", deixe no mesmo deiretório.
 
 :: Habilitar o uso de cores personalizadas. Use a lógica booleana, ou seja,
-:: 1 representa "true" e 0 representa "false", o padrão é 0 
+:: 1 representa "true" e 0 representa "false", o valor padrão é 0 
 set allow_custom_colors=1
 
 REM Adicionado cores personalizadas
 
-:: Você definer uaa cor especifíca para cada questão/pergunta, e lembre-se, só
-:: funcionar se a variável acima "allow_custom_colors" = 1 (boolean).
+:: Você pode definer uma cor especifíca para cada questão/pergunta, e lembre-se, 
+:: só funciona se a variável acima "allow_custom_colors" = 1 (boolean).
 
 :: Remova o comentário das variáveis abaixo e adicione a sua cor favorita.
 :: Minha recomendação é que você veja as opções de cores disponíveis, use: 
